9ac60816cfc78dc68129295bbca490c77cff6cc9
jnicolet <joakimnicolet@gmail.com>
2021-06-25 10:52:14 +0200

    Add ut for miscUtils_extendSignBit64(), miscUtils_extendSignBit32(), miscUtils_extendSignBit16() and miscUtils_extendSignBit8()

--- test/gtest/ut_miscUtils.cpp
+++ test/gtest/ut_miscUtils.cpp
@@ -77,11 +77,118 @@ TEST(BigEndianBytesToVariableTest, ToUint16NullPointer)
     EXPECT_FALSE(miscUtils_bigEndianBytesToUint16(buffer.data(), NULL));
 }
 
-TEST(SignExtendTest, Extend16)
+TEST(ExtendSignBitTest, extendSignBit64)
 {
-    int16_t value = 0x02AB;
+    int64_t value = 0x2A;
+    int64_t result = 0;
+
+    EXPECT_TRUE(miscUtils_extendSignBit64(value, 6, &result));
+    EXPECT_EQ(0xFFFFFFFFFFFFFFEA, (uint64_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit64(value, 7, &result));
+    EXPECT_EQ(0x2A, (uint64_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit64(value, 5, &result));
+    EXPECT_EQ(0xA, (uint64_t)result);
+}
+
+TEST(ExtendSignBitTest, extendSignBit64InvalidInputs)
+{
+    int64_t value = 0;
+    int64_t result = 10;
+
+    EXPECT_FALSE(miscUtils_extendSignBit64(value, 64, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit64(value, 1, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit64(value, 5, NULL));
+}
+
+TEST(ExtendSignBitTest, extendSignBit32)
+{
+    int32_t value = 0x2A;
+    int32_t result = 0;
+
+    EXPECT_TRUE(miscUtils_extendSignBit32(value, 6, &result));
+    EXPECT_EQ(0xFFFFFFEA, (uint32_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit32(value, 7, &result));
+    EXPECT_EQ(0x2A, (uint32_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit32(value, 5, &result));
+    EXPECT_EQ(0xA, (uint32_t)result);
+}
+
+TEST(ExtendSignBitTest, extendSignBit32InvalidInputs)
+{
+    int32_t value = 0;
+    int32_t result = 10;
+
+    EXPECT_FALSE(miscUtils_extendSignBit32(value, 32, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit32(value, 1, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit32(value, 5, NULL));
+}
+
+TEST(ExtendSignBitTest, extendSignBit16)
+{
+    int16_t value = 0x2A;
     int16_t result = 0;
 
-    EXPECT_TRUE(miscUtils_extendSignBit16(value, 10, &result));
-    EXPECT_EQ(0xFEAB, (uint16_t) result);
+    EXPECT_TRUE(miscUtils_extendSignBit16(value, 6, &result));
+    EXPECT_EQ(0xFFEA, (uint16_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit16(value, 7, &result));
+    EXPECT_EQ(0x2A, (uint16_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit16(value, 5, &result));
+    EXPECT_EQ(0xA, (uint16_t)result);
+}
+
+TEST(ExtendSignBitTest, extendSignBit16InvalidInputs)
+{
+    int16_t value = 0;
+    int16_t result = 10;
+
+    EXPECT_FALSE(miscUtils_extendSignBit16(value, 16, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit16(value, 1, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit16(value, 5, NULL));
+}
+
+TEST(ExtendSignBitTest, extendSignBit8)
+{
+    int8_t value = 0x2A;
+    int8_t result = 0;
+
+    EXPECT_TRUE(miscUtils_extendSignBit8(value, 6, &result));
+    EXPECT_EQ(0xEA, (uint8_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit8(value, 7, &result));
+    EXPECT_EQ(0x2A, (uint8_t)result);
+
+    EXPECT_TRUE(miscUtils_extendSignBit8(value, 5, &result));
+    EXPECT_EQ(0xA, (uint8_t)result);
+}
+
+TEST(ExtendSignBitTest, extendSignBit8InvalidInputs)
+{
+    int8_t value = 0;
+    int8_t result = 10;
+
+    EXPECT_FALSE(miscUtils_extendSignBit8(value, 8, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit8(value, 1, &result));
+    EXPECT_EQ(10, result);
+
+    EXPECT_FALSE(miscUtils_extendSignBit8(value, 5, NULL));
 }
